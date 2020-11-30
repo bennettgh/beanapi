5343efaa4fab6857a235ac24ec5507dde12e9cf8
Andrew Bennett <artosis@Andrews-MacBook-Pro.local>
2020-11-30 17:51:33 -0500

    zoop

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,4 +1,4 @@
-const stuff = () => {
+exports.handler = async (event, context) => {
   const today = new Date()
   const todaysDate = today.getDate();
 
@@ -119,34 +119,5 @@ const stuff = () => {
       "Access-Control-Allow-Origin": "*"
     },
     body: JSON.stringify(response)
-  };
-};
-
-
-// src/customers.js
-exports.handler = async (event, context) => {
-  const path = event.path.replace(/\.netlify\/functions\/[^\/]+/, '');
-  const segments = path.split('/').filter(e => e);
-
-  switch (event.httpMethod) {
-    case 'GET':
-      return stuff()
-
-    case 'OPTIONS':
-      // To enable CORS
-      const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-      };
-      return {
-        statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
-        headers,
-        body: 'This was a preflight call!'
-      };
-  }
-  return {
-    statusCode: 500,
-    body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE/OPTIONS'
   };
 };
